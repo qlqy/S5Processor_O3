bb87e25f5d6ca663c716f915f599c7b6733f2439
qlqy <61494332+qlqy@users.noreply.github.com>
2021-03-19 18:48:41 +0800

    Add files via upload

---
+++ R/S5Processor.R
@@ -0,0 +1,20 @@
+#' Sentinel-5P L2 Processing Tool
+#'
+#' This R package automatically converts the Sentinel-5P L2 NetCDF files into TIFF files.
+#' The user can define the product which should be turned into a TIFF and also mosaic from
+#' a list of file-paths.
+#'
+#' @details Function included in this package:
+#' \itemize{
+#'   \item \code{\link{S5P_process}}
+#' }
+#'
+#' @docType package
+#'
+#' @author Marius Philipp \email{marius.philipp@stud-mail.uni-wuerzburg.de}
+#'
+#' @references This program was written at Remote Sensing Solutions RSS GmbH,
+#'             Dingolfinger Str. 9, 81673 Munich, Germany \url{https://rssgmbh.de/}.
+#'
+#' @name S5Processor
+NULL
